4307627b1fa566de9089ad1bbf7bf1cedd41554f
rahulkusukuntla234 <rahulkusukuntla234@gmail.com>
2024-08-27 11:22:17 +0530

    ME_QA_XYOUTUBE_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,5 +1,6 @@
 package demo;
 
+import org.apache.xmlbeans.impl.xb.xsdschema.ListDocument.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
@@ -15,7 +16,6 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-
 import java.time.Duration;
 import java.util.logging.Level;
 
@@ -31,6 +31,99 @@ public class TestCases extends ExcelDataProvider{ // Lets us read the data
          * Follow `testCase01` `testCase02`... format or what is provided in
          * instructions
          */
+        @Test
+        public void testCase01(){
+                driver.get("https://www.youtube.com/");
+                
+                SoftAssert softAssert = new SoftAssert();
+                String url=driver.getCurrentUrl();
+                
+                String actual="https://www.youtube.com/";
+                Wrappers.sAssert(url,actual,"URL not matched");
+                
+                Wrappers.clickAbout(driver);
+                // driver.findElement(By.xpath("//a[text()='About']")).click();
+                
+                url=driver.getCurrentUrl();
+                actual="https://about.youtube/";
+                Wrappers.sAssert(url,actual,"About URL not matched");
+                String aboutText=Wrappers.getAboutContent(driver);
+                System.out.println(aboutText);
+
+        }
+        @Test
+        public void testCase02(){
+                driver.get("https://www.youtube.com/");
+                Wrappers.clickMovies(driver);
+                WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(5));
+                wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@aria-label='Next']")));
+                WebElement nextButton=driver.findElement(By.xpath("//button[@aria-label='Next']"));
+                while(nextButton.isDisplayed()){
+                        nextButton.click();
+                }
+                String rating=Wrappers.getMovieRating(driver);
+                Wrappers.sAssert(rating,"U","Rating not matched");
+                String category=Wrappers.getMovieCategory(driver);
+                Wrappers.sAssertTrue(category.contains("Animation"), "Category not matched");
+
+        }
+        @Test
+        public void testCase03(){
+                driver.get("https://www.youtube.com/");
+                Wrappers.clickMusic(driver);
+                WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(5));
+                wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//button[@aria-label='Next'])[1]")));
+                WebElement nextButton=driver.findElement(By.xpath("(//button[@aria-label='Next'])[1]"));
+                while(nextButton.isDisplayed()){
+                        nextButton.click();
+                }
+                String playList=Wrappers.getPlaylist(driver);
+                Wrappers.sAssert(playList,"Bollywood Dance Hitlist","PlayList not matched");
+                String noOfTracks=Wrappers.getNoOfTracks(driver);
+                int tracks=Integer.parseInt(noOfTracks.substring(0, 2));
+                Wrappers.sAssertTrue(tracks<=50, "Tracks not matched");
+                
+        }
+        @Test
+        public void testCase04(){
+                driver.get("https://www.youtube.com/");
+                Wrappers.clickNews(driver);
+                WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(5));
+                wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//span[text()='Latest news posts']/ancestor::div[6]//div[@id='contents']/ytd-rich-item-renderer)[1]")));
+                java.util.List<WebElement> list=driver.findElements(By.xpath("//span[text()='Latest news posts']/ancestor::div[6]//div[@id='contents']/ytd-rich-item-renderer"));
+                String n="";
+                long v=0;
+                for(int i=0;i<3;i++){
+                       String s= list.get(i).findElement(By.xpath(".//div[@id='header']/div[2]")).getText();
+                       String q=list.get(i).findElement(By.xpath(".//div[@id='body']//yt-formatted-string[@id='home-content-text']/span[1]")).getText();
+                       System.out.println(s);
+                       System.out.println(q);
+                       n=list.get(i).findElement(By.xpath(".//div[@id='toolbar']//span[@id='vote-count-middle']")).getText();
+                       System.out.println(n);
+                       v+=Wrappers.getLikes(n);
+                        
+                }
+                System.out.println(v);
+                
+                
+        }
+        @Test(dataProvider="excelData")
+        public void testCase05(String toBeSearched){
+                driver.get("https://www.youtube.com/");
+                Wrappers.toBeSearched(driver, toBeSearched);
+                java.util.List<WebElement> view=driver.findElements(By.xpath("//div[@class='text-wrapper style-scope ytd-video-renderer']//span[@class='inline-metadata-item style-scope ytd-video-meta-block'][1]"));
+                long j=0;
+                for(WebElement ele:view){
+                        Wrappers.scrollTo(driver, ele);
+                        String d=ele.getText();
+                        j+=Wrappers.getLikes(d.substring(0, d.length()-6));
+                        if(j>100000000){
+                                break;
+                        }
+                }
+                
+                
+        }
 
         /*
          * Do not change the provided methods unless necessary, they will help in
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -6,12 +6,141 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.asserts.SoftAssert;
+
 import java.time.Duration;
 
 public class Wrappers {
+    
     /*
      * Write your selenium wrappers here
      */
+    // static ChromeDriver driver;
+    
+    
+    public static void sAssert(String actual,String expected,String message){
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(actual,expected,message);
+        softAssert.assertAll();
+    }
+    public static void sAssertTrue(boolean actual,String message){
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(actual,message);
+        softAssert.assertAll();
+    }
+
+    public static void clickAbout(ChromeDriver driver){
+        
+        WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='About']")));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        WebElement element=driver.findElement(By.xpath("//a[text()='About']"));
+        js.executeScript("arguments[0].scrollIntoView();", element);
+        element.click();
+    }
+    public static String getAboutContent(ChromeDriver driver){
+        WebElement element=driver.findElement(By.xpath("//section[@class='ytabout__content']"));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView();", element);
+        String text=element.getText();
+        return text;
+    } 
+    public static void clickMovies(ChromeDriver driver){
+        
+        WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//a[@id='endpoint'])[9]")));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        WebElement element=driver.findElement(By.xpath("(//a[@id='endpoint'])[9]"));
+        js.executeScript("arguments[0].scrollIntoView();", element);
+        element.click();
+    }
+    public static String getMovieRating(ChromeDriver driver){
+        WebElement element=driver.findElement(By.xpath("(//div[@class='badge  badge-style-type-simple style-scope ytd-badge-supported-renderer style-scope ytd-badge-supported-renderer'])[16]/p"));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView();", element);
+        String text=element.getText();
+        return text;
+    }
+    public static String getMovieCategory(ChromeDriver driver){
+        WebElement element=driver.findElement(By.xpath("(//span[@class='grid-movie-renderer-metadata style-scope ytd-grid-movie-renderer'])[16]"));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView();", element);
+        String text=element.getText();
+        return text;
+    }
+    public static void clickMusic(ChromeDriver driver){
+        
+        WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@title='Music']")));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        WebElement element=driver.findElement(By.xpath("//a[@title='Music']"));
+        js.executeScript("arguments[0].scrollIntoView();", element);
+        element.click();
+    }
+    public static String getPlaylist(ChromeDriver driver){
+        WebElement element=driver.findElement(By.xpath("(//a[@class='yt-simple-endpoint style-scope ytd-compact-station-renderer'])[11]/h3"));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView();", element);
+        String text=element.getText();
+        return text;
+    }
+    public static String getNoOfTracks(ChromeDriver driver){
+        WebElement element=driver.findElement(By.xpath("(//a[@class='yt-simple-endpoint style-scope ytd-compact-station-renderer'])[11]/p"));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView();", element);
+        String text=element.getText();
+        return text;
+    }
+    public static void clickNews(ChromeDriver driver){
+        
+        WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@title='News']")));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        WebElement element=driver.findElement(By.xpath("//a[@title='News']"));
+        js.executeScript("arguments[0].scrollIntoView();", element);
+        element.click();
+    }
+    public static void scrollTo(ChromeDriver driver,WebElement element){
+        
+       
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        
+        js.executeScript("arguments[0].scrollIntoView();", element);
+        
+    }
+
+    public static long getLikes(String str){
+        if(!str.isEmpty()){
+            if(str.charAt(str.length()-1)=='K'){
+                float f=Float.parseFloat(str.substring(0, str.length()-1));
+                return (long) (f*1000);
+            }
+            else if(str.charAt(str.length()-1)=='M'){
+                float f=Float.parseFloat(str.substring(0, str.length()-1));
+                return (long) (f*1000000);
+            }
+            else{
+                return Long.parseLong(str);
+            }
+        }
+        return 0;
+        
+    }
+    public static void toBeSearched(ChromeDriver driver,String key){
+        
+        WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='search']")));
+        
+        WebElement element=driver.findElement(By.xpath("//input[@id='search']"));
+        
+        element.sendKeys(key);
+        WebElement search=driver.findElement(By.xpath("//button[@id='search-icon-legacy']"));
+        search.click();
+    }
+
+
+
 }
